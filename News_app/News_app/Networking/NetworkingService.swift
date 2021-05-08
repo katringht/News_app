4c97a6da08d23a2b235f0a0aec89080a89991752
katringht <katrin_tko@mail.ru>
2021-05-08 12:25:21 +0300

    add background thread

--- News_app/News_app/Networking/NetworkingService.swift
+++ News_app/News_app/Networking/NetworkingService.swift
@@ -18,21 +18,29 @@ class NetworkingService {
 
     func getDataWith(completion: @escaping (Result<[[String: AnyObject]]>) -> Void) {
         
-        let urlString = endPoint
+        let URLString = endPoint
         
-        guard let url = URL(string: urlString) else { return completion(.Error("Invalid URL, we can't update your feed")) }
+        guard let isURL = URL(string: URLString) else {
+            return completion(.Error("Invalid URL, we can't update your feed"))
+        }
 
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
+        URLSession.shared.dataTask(with: isURL) { (data, response, error) in
             
-         guard error == nil else { return completion(.Error(error!.localizedDescription)) }
-            guard let data = data else { return completion(.Error(error?.localizedDescription ?? "There are no new Items to show"))
-}
+         guard error == nil else {
+            return completion(.Error(error!.localizedDescription))
+         }
+            guard let data = data else {
+                return completion(.Error(error?.localizedDescription ?? "There are no new Items to show"))
+            }
+            
             do {
                 if let json = try JSONSerialization.jsonObject(with: data, options: [.mutableContainers]) as? [String: AnyObject] {
                     guard let itemsJsonArray = json["results"] as? [[String: AnyObject]] else {
                         return completion(.Error(error?.localizedDescription ?? "There are no new Items to show"))
                     }
-                    DispatchQueue.main.async {
+                    
+                    let dispatchQueue = DispatchQueue(label: "QueueIdentification", qos: .background)
+                    dispatchQueue.async {
                         completion(.Success(itemsJsonArray))
                     }
                 }
